91267301d342d16efd2d3c63cb2e4e231844efec
Rohan <devx.rohan@gmail.com>
2025-01-13 23:43:59 +0530

    added login and register from also set up concurrent package

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Form, Input } from "antd";
+import '../styles/RegisterStyles.css'
+import {Link} from 'react-router-dom';
 
 const Login = () => {
+
+  //Form Handler
+  const onFinishHandler = (values) => {
+    console.log(values);
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  )
-}
+    <>
+      <div className="form-container">
+        <Form layout="vertical" onFinish={onFinishHandler} className="register-form">
+            <h3 className='h3' >Login</h3>
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" required />
+          </Form.Item>
+          <Link to='/register' className="m-4" >New User</Link>
+          <button id="btn" className="btn btn-primary" type="submit" >Login</button>
+        </Form>
+      </div>
+    </>
+  );
+};
 
 export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Button, Checkbox, Form, Input } from "antd";
 import '../styles/RegisterStyles.css'
+import {Link} from 'react-router-dom';
 
 const Register = () => {
+
   //Form Handler
   const onFinishHandler = (values) => {
     console.log(values);
@@ -22,6 +24,7 @@ const Register = () => {
           <Form.Item label="Password" name="password">
             <Input type="password" required />
           </Form.Item>
+          <Link to='/login' className="m-4" >Existing User</Link>
           <button id="btn" className="btn btn-primary" type="submit" >Sign Up</button>
         </Form>
       </div>
